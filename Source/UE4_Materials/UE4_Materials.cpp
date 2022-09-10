ee33374ce6b8552ca48ffa4160b71cac9afbf234
lai001 <37958940+lai001@users.noreply.github.com>
2022-09-11 01:11:19 +0800

    Initial commit

---
+++ Source/UE4_Materials/UE4_Materials.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE4_Materials.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4_Materials, "UE4_Materials" );
